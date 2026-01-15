f869cee43d77328fad0fc782b7c899085953b248
ghddbsrl1234 <qoxmfdbsrl@gamil.com>
2026-01-15 16:01:03 +0900

    2026/01/15

---
+++ boardProject/src/main/java/edu/kh/project/admin/model/service/AdminService.java
@@ -0,0 +1,26 @@
+package edu.kh.project.admin.model.service;
+
+import java.util.List;
+import edu.kh.project.board.model.dto.Board;
+import edu.kh.project.member.model.dto.Member;
+
+public interface AdminService {
+
+	Member login(Member inputMember);
+
+	int checkEmail(String memberEmail);
+
+	String createAdminAccount(Member member);
+
+	List<Member> adminAccountList();
+
+	Board maxReadCount();
+
+	Board maxLikeCount();
+
+	Board maxCommentCount();
+
+	List<Member> selectWithdrawnMemberList();
+
+	int restoreMember(int memberNo);
+}
--- boardProject/src/main/java/edu/kh/project/board/model/dto/Board.java
+++ boardProject/src/main/java/edu/kh/project/board/model/dto/Board.java
@@ -7,12 +7,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Board {
-
+	// BOARD 테이블 컬럼
 	private int boardNo;
 	private String boardTitle;
 	private String boardContent;
@@ -28,7 +28,8 @@ public class Board {
 	
 	// 목록 조회 시 서브쿼리 필드
 	private int commentCount; // 댓글 수
-	private int likeCount; // 좋아요 수
+	private int likeCount;    // 좋아요 수
+	
 	
 	// 게시글 작성자 프로필 이미지
 	private String profileImg;
@@ -39,10 +40,24 @@ public class Board {
 	// 게시글 좋아요 여부 확인
 	private int likeCheck;
 	
-	// 게시글에 등록된 이미지 목록
+	// 게시글에 등록된 이미지 목록 
 	private List<BoardImg> imageList;
 	
 	// 게시글에 등록된 댓글 목록
 	private List<Comment> commentList;
+	
+	// ---- React 관리자페이지 필요 ----
+	// 게시판 종류명
+	private String boardName;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
